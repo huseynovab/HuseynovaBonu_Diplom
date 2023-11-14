4fdb67a14ab674c84e58ea735dc0b4f8a31446ef
huseynovab <bonu0498@yandex.ru>
2023-11-14 21:06:09 +0300

    my second commit

---
+++ Data.py
@@ -0,0 +1,13 @@
+User_body = {
+    "firstName": "Bonu",
+    "lastName": "huseynova",
+    "address": "Spain",
+    "metroStation": 4,
+    "phone": "+7 800 800 80 80",
+    "rentTime": 5,
+    "deliveryDate": "2023-10-10",
+    "comment": "Hello mir",
+    "color": [
+        "BLACK"
+    ]
+}
